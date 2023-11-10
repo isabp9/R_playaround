164020d6beea1b709e88199f051f4359a6527482
Isabella Pau <isabella.pau1999@gmail.com>
2023-11-10 09:14:25 +0100

    Add tutorial git and github

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,1 +1,11 @@
 library(tidyverse)
+
+## already connected R with git and github and own account. 
+## now to create new project and connect it to git:
+
+#library(usethis)
+#use_git()
+## then use right pane "Git" and commit from there to save changes 
+
+## to create repository for the project in github
+#use_github()
